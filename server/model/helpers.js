30a59d948e90d72561d4315797972c8c4785b3d6
Patrick Noda <patricknoda11@gmail.com>
2022-05-03 02:26:12 -0700

    fixed jsdoc for server helpers

--- server/model/helpers.js
+++ server/model/helpers.js
@@ -1,7 +1,7 @@
 /**
- * removes all leading/trailing whitespace characters and converts every character into lowercase
- * @param {string} text
- * @returns string
+ * Removes all leading/trailing whitespace characters and converts every character into lowercase
+ * @param {string} text - the text to be refined
+ * @returns {string} the refined text
  */
 const refineText = (text) => {
     return text.trim().toLowerCase();
